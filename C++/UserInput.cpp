d4273c9fbdf8a0a97ea731a4bf9f078c967d6341
Ben Kiev <146984941+ItsCrazyWolfWasTaken@users.noreply.github.com>
2025-03-30 16:55:41 -0400

    added a funtime if statement tee hee hee

--- C++/UserInput.cpp
+++ C++/UserInput.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int main(){
-    string ssn;
+    int ssn;
     cout << "Hello Bozos, give me your social security number. Now. Or you die. I have a Glock-18C. It has Happy Mode." << endl;
     cin >> ssn;
-    cout << ssn << ", is this true? No? Ok, now you die. *pew pew pew*" <<endl; 
+    if (ssn == 0) {
+        cout << "You no give me your social security, now you die. *pew pew pew*" << endl;
+    }
+    else {
+        cout << ssn << ", is this true? No? Ok, now you die. *pew pew pew*" << endl;
+    }
     return 0;
 }
