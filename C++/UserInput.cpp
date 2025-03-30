0708b4182bc7db9570a7d99faa8905d8354c7aed
Ben Kiev <146984941+ItsCrazyWolfWasTaken@users.noreply.github.com>
2025-03-30 16:51:46 -0400

    addeds userinput coode from the cpp ccp

---
+++ C++/UserInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string ssn;
+    cout << "Hello Bozos, give me your social security number. Now. Or you die. I have a Glock-18C. It has Happy Mode." << endl;
+    cin >> ssn;
+    cout << ssn << ", is this true? No? Ok, now you die. *pew pew pew*" <<endl; 
+    return 0;
+}
