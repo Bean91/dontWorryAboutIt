fac4e55d672a2ad1131300b30ed1ec1a63a597b4
Ben Kiev <146984941+ItsCrazyWolfWasTaken@users.noreply.github.com>
2025-04-13 11:03:42 -0400

    test

--- C++/HelloWorld.cpp
+++ C++/HelloWorld.cpp
@@ -1,8 +1,8 @@
 #include <iostream> //Activates Input-Output Stream
 using namespace std; //A namespace is like a library, it allows you to use functions, classes, and objects.
 
-int main(){
-    string hellomofos = "Hello World Bozos";
-    cout << hellomofos << endl;
-    return 0;
-}
+// int main(){
+//     string hellomofos = "Hello World Bozos";
+//     cout << hellomofos << endl;
+//     return 0;
+// }
